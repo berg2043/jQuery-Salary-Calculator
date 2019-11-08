aeaa71d43e25938a731fa7654e048aaccbb79545
Phillip Berg <phillipb1991@msn.com>
2019-11-08 15:50:59 -0600

    added budget chacker that highlights total if above 20k

--- script.js
+++ script.js
@@ -53,8 +53,17 @@ function totalSalaryCalc(){
     totalSalary += Number(employee.salary);
   }
   $('#total').text(totalSalary);
+  budgetChecker(totalSalary);
 }
 
+// Checks to make sure sum is below $20k and highlights total if above $20k
+function budgetChecker(totals){
+  if(totals > 20000){
+    $('#monthly-total').addClass('red');
+  } else {
+    $('#monthly-total').removeClass('red');
+  }
+}
 // Combines functionality into the submit button
 function clickSubmit(event){
   event.preventDefault();
@@ -63,11 +72,6 @@ function clickSubmit(event){
   pushToTable(newEmployee);
   totalSalaryCalc();
 }
-// A 'Submit' button should collect the form information, store the information to 
-// calculate monthly costs, append information to the DOM and clear the input 
-// fields. Using the stored information, calculate monthly costs and append this 
-// to the to DOM. If the total monthly cost exceeds $20,000, add a red background 
-// color to the total monthly cost.
 
 // Create a delete button that removes an employee from the DOM. For Base mode, it 
 // does not need to remove that Employee's salary from the reported total.
